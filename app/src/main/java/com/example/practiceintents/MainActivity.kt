089221a371d9dc69b7227f971858939fad0099b5
miroks404 <themiroks@gmail.com>
2025-02-15 20:03:18 +0200

    intents

--- app/src/main/java/com/example/practiceintents/MainActivity.kt
+++ app/src/main/java/com/example/practiceintents/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.practiceintents
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.practiceintents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bTime.setOnClickListener {
+            val intent = Intent("com.example.practiceintents.SHOW_TIME")
+            startActivity(intent)
+        }
+
+        binding.bDate.setOnClickListener {
+            val intent = Intent("com.example.practiceintents.SHOW_DATE")
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/practiceintents/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.practiceintents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.practiceintents.databinding.ActivitySecondBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var timeFormat: String
+
+        binding.tvDateOrTime.text = intent?.action?.let {
+                when(it) {
+                    "com.example.practiceintents.SHOW_TIME" -> {
+                        timeFormat = "HH:mm:ss"
+                        "Time: ${SimpleDateFormat(timeFormat).format(Date(System.currentTimeMillis()))}"
+                    }
+                    "com.example.practiceintents.SHOW_DATE" -> {
+                        timeFormat = "dd.MM.yyyy"
+                        "Date: ${SimpleDateFormat(timeFormat).format(Date(System.currentTimeMillis()))}"
+                    }
+                    else -> "Error"
+                }
+            }
+
+
+    }
+}
